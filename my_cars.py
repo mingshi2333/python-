dad4689073f63dbaa5549007dfd450907466e235
mingshi2333 <1448513978@qq.com>
2018-12-04 21:55:01 +0800

    Add files via upload

---
+++ my_cars.py
@@ -0,0 +1,8 @@
+from car import Car,ElectricCar
+my_beetle = Car('vsdds','dsf','2016')
+print(my_beetle.get_descriptive_name())
+
+my_tesla =ElectricCar('ds','ada',2016)
+print(my_tesla.get_descriptive_name())
+
+
